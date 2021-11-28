4f933894b528093b142afc4964d9f1b8ac4a5ba7
tigersv <ttiiggeerrssvv@gmail.com>
2021-11-28 13:01:54 +0300

    LeftContent Add image card

---
+++ src/components/References/MyPage/Main/Content/LeftContent.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Card, Button} from "react-bootstrap";
+import userImg from '../../../../../assets/images/user.png'
+
+const LeftContent = () => {
+    return (
+        <div>
+            <Card className="text-center">
+                <Card.Img variant="top" src={userImg} />
+                <Card.Body>
+                    <Button variant="primary">Редактировать</Button>
+                    <div style={{"marginTop": 20, "marginBottom": 20}}></div>
+                    <Card.Text>
+                        Воспоминания
+                    </Card.Text>
+                    <Card.Text>
+                        Денежные перводы
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default LeftContent;
---
+++ src/components/References/MyPage/Main/Content/RightContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightContent = () => {
+    return (
+        <div>
+            RightContent
+        </div>
+    );
+};
+
+export default RightContent;
