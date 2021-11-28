4b14b286ebf211b0510a4a98d41176102ce57fb6
tigersv <ttiiggeerrssvv@gmail.com>
2021-11-28 11:56:41 +0300

    Set the bootstrap +MyPage +News

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import React from "react";
+import MyPage from "./components/References/MyPage/MyPage";
+import News from "./components/References/News/News";
 
 function App() {
   return (
     <div>
-        App
+        <MyPage/>
+        {/*<News/>*/}
     </div>
   );
 }
