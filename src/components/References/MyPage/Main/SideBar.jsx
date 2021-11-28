3a7b1c3d59aeb445f57853e28b51f8848b12c74d
Nikita <nsahov111@mail.ru>
2021-11-28 13:23:33 +0300

    added Sidebar

--- src/components/References/MyPage/Main/SideBar.jsx
+++ src/components/References/MyPage/Main/SideBar.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const SideBar = () => {
     return (
         <div>
-            SideBar
+            <Nav className="flex-column">
+                <Nav.Link eventKey="link-1" href="/my_page">моя страница</Nav.Link>
+                <Nav.Link eventKey="link-2" href="message">сообщения</Nav.Link>
+                <Nav.Link eventKey="link-3" href="friends">друзья</Nav.Link>
+                <Nav.Link eventKey="link-4" href="/news">новости</Nav.Link>
+                <Nav.Link eventKey="link-5" href="community">сообщества</Nav.Link>
+            </Nav>
         </div>
     );
 };
