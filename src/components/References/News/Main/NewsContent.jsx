5f0d9e2d9819122833d61558bf90cb87b269b026
tigersv <ttiiggeerrssvv@gmail.com>
2021-11-29 10:42:24 +0300

    Comments to news

--- src/components/References/News/Main/NewsContent.jsx
+++ src/components/References/News/Main/NewsContent.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import {Card, ListGroup, ListGroupItem} from "react-bootstrap"
+/*
+В целом у тебя это должно выглядеть как то так -
+после импорта делаеш массив данных новостей
+Пример:
+const newsList = [
+    {
+        title: "Some title",
+        text: "Some text",
+        author: "Some author",
+        category: "Some category"
+    },
+    {
+        title2: "Some title2",
+        text2: "Some text2",
+        author2: "Some author2",
+        category: "Some category"
+    } ...
+]
+трех будет достаточно.
+Потом делаешь внешний вид карточки,
+и отрисовываеш их с помощью newsList.map()
+Лучшей практикой будет сделать отдельную функцию renderNews()
+*/
 
 const NewsContent = () => {
     return (
         <div>
+            {/*Карточку можно растянуть
+            (style={{ width: '100%' }})*/}
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
                 <Card.Body>
