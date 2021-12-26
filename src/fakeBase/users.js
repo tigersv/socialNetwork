8ed477fc8123c2b4cf07fb28a9a17282a56f1aa5
tigersv <ttiiggeerrssvv@gmail.com>
2021-12-26 12:11:14 +0300

    Adding rendering Users

--- src/fakeBase/users.js
+++ src/fakeBase/users.js
@@ -26,4 +26,22 @@ export const users = [
         course: 3,
         online: false
     },
+    {
+        id: 4,
+        name: "Bob",
+        username: "Bob",
+        avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2umeiV8pVdahTlB81yAxw4b8O7X0PFaEDsQ&usqp=CAU",
+        email: "bob@yahoo.com",
+        course: 3,
+        online: false
+    },
+    {
+        id: 5,
+        name: "Bob",
+        username: "Bob",
+        avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2umeiV8pVdahTlB81yAxw4b8O7X0PFaEDsQ&usqp=CAU",
+        email: "bob@yahoo.com",
+        course: 3,
+        online: false
+    },
 ]
