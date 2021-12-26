7fb039ffc2c008fe32b46aaec326a512a3dacddf
tigersv <ttiiggeerrssvv@gmail.com>
2021-12-26 10:46:07 +0300

    Adding FakeBase

---
+++ src/fakeBase/users.js
@@ -0,0 +1,26 @@
+export const users = [
+    {
+        id: 1,
+        name: "Nikita",
+        username: "Nik",
+        avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2umeiV8pVdahTlB81yAxw4b8O7X0PFaEDsQ&usqp=CAU",
+        email: "Nik@mail.ru",
+        course: 1
+    },
+    {
+        id: 2,
+        name: "Igor",
+        username: "Garry",
+        avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2umeiV8pVdahTlB81yAxw4b8O7X0PFaEDsQ&usqp=CAU",
+        email: "garry@gmail.com",
+        course: 2
+    },
+    {
+        id: 3,
+        name: "Bob",
+        username: "Bob",
+        avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2umeiV8pVdahTlB81yAxw4b8O7X0PFaEDsQ&usqp=CAU",
+        email: "bob@yahoo.com",
+        course: 3
+    },
+]
