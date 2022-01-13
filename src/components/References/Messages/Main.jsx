23de16339eb86a2ec01430600eb258dd4bb131db
NikTikJK <nsahov111@mail.ru>
2022-01-13 11:35:04 +0300

    added adaptability

--- src/components/References/Messages/Main.jsx
+++ src/components/References/Messages/Main.jsx
@@ -8,13 +8,13 @@ const Main = () => {
     const yourFriends = () => {
         return(users.map((mappedObject, index) => {
             return(
-                <Card style={{ width: '50rem', borderRadius: '50px', margin: '0px 0px 5px 0px',}} id={mappedObject.id}>
-                    <p style={{margin: '5px 0px 5px 15px'}}>
-                        <Card.Img variant="top" src={mappedObject.avatar} style={{width: '5rem', float: "left", verticalAlign: "middle", margin: '10px 20px 10px 0px'}} />
+                <Card style={{ width: '100%', borderRadius: '50px', margin: '0px 0px 5px 0px',}} id={mappedObject.id}>
+                    <p style={{margin: '2% 0px 2% 2%'}}>
+                        <Card.Img variant="top" src={mappedObject.avatar} style={{width: '9.5%', float: "left", verticalAlign: "middle", margin: '1% 5% 2% 0px'}} />
                         <p style={{textAlign: 'center'}}>{mappedObject.name}</p>
                         <div style={{verticalAlign: "bottom",}}>
                         <button style={{
-                            backgroundColor: '#e0e0e0', width: '40rem', margin: '0px auto 0px ', height: "50px", border: "0", borderRadius: "20px"
+                            backgroundColor: '#e0e0e0', width: '80% ', margin: '0px auto 0px ', height: "50px", border: "0", borderRadius: "20px"
                         }}>
                         </button>
                         </div>
